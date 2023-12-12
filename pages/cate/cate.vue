5e8c7d84ff28f89fa5c7c78c0e55f3a8d88a7788
Eason <guozhongpeng.11@gmail.com>
2023-12-12 16:33:59 +0800

    完成了cate页面的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,86 @@
 <template>
-	<view>
-		cate
+	<view class='scroll-view-wrapper'>
+		<scroll-view class='scroll-view-left' scroll-y :style='{height:wh + "px"}'>
+			<view class='left-scroll-view-item active'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+			<view class='left-scroll-view-item'>left</view>
+		</scroll-view>
+		
+		<scroll-view class='scroll-view-right' scroll-y :style='{height:wh + "px"}'>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+			<view>right</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -8,12 +88,41 @@
 	export default {
 		data() {
 			return {
-				
+				wh:0
 			};
+		},
+		onLoad(){
+			const sysInfo=uni.getSystemInfoSync()
+			this.wh=sysInfo.windowHeight
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	.scroll-view-wrapper{
+		display:flex
+	}
+	.left-scroll-view-item{
+		background-color: #f7f7f7;
+		line-height:60px;
+		text-align:center;
+		font-size:12px;
+	
+		&.active{
+			background-color: #fff;
+			position:relative;
+			
+			&::before{
+				content:'';
+				display:block;
+				width: 3px;
+				height:30px;
+				background-color: #c00000;
+				position:absolute;
+				top:50%;
+				left:0;
+				transform:translateY(-50%)
+			}
+		}
+	}
 </style>
